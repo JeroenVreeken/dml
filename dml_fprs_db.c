c1308e778e67dce3de686c4b3a4bea5819d4b5ec
Jeroen Vreeken <jeroen@vreeken.net>
2017-01-22 13:55:58 +0100

    fix priv usage in fprs db

--- dml_fprs_db.c
+++ dml_fprs_db.c
@@ -155,11 +155,11 @@ void rx_packet(struct dml_connection *dc, void *arg,
 			uint8_t *rid = dml_stream_id_get(ds);
 			if (!strcmp(DML_MIME_FPRS, dmime)) {
 				struct dml_stream_priv *priv = dml_stream_priv_get(ds);
-				priv->match_mime = true;
 				if (!priv) {
 					priv = stream_priv_new();
 					dml_stream_priv_set(ds, priv);
 				}				
+				priv->match_mime = true;
 				struct dml_crypto_key *ck = dml_stream_crypto_get(ds);
 				if (!ck)
 					dml_packet_send_req_certificate(dc, rid);
