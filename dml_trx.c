b66f2944570dd526b052a3ea02b7f4be6351acb6
Jeroen Vreeken <jeroen@vreeken.net>
2017-05-24 03:02:38 +0200

    Send DML status to aprsis

--- dml_trx.c
+++ dml_trx.c
@@ -157,6 +157,10 @@ static int fprs_update_status(char *stream, char *assoc)
 	/* Send FPRS frame with callsign in FreeDV header */
 	trx_dv_send_fprs(mac_dev, mac_bcast, dml_data, dml_size);
 
+	if (aprsis) {
+		fprs_aprsis_frame(fprs_frame, mac_dev);
+	}
+
 	/* Add callsign to packet for others */
 	fprs_frame_add_callsign(fprs_frame, mac_dev);
 
