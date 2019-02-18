52750ce36e5599cdc1a78fb98ce0e914ef317888
Jeroen Vreeken <jeroen@vreeken.net>
2019-02-18 20:14:47 +0100

    Build fixes

--- dml_streamer.c
+++ dml_streamer.c
@@ -167,7 +167,7 @@ void send_data(void *data, size_t size)
 	timestamp = (uint64_t)ts.tv_sec << 16;
 	timestamp |= prev_ctr;
 	
-	printf("timestamp: 0x%016llx\n", timestamp);
+	printf("timestamp: 0x%016"PRIx64"\n", timestamp);
 	dml_packet_send_data(dml_con, packet_id, data, size, timestamp, dk);
 }
 
